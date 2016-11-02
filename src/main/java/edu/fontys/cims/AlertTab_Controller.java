a903636d99603fbd8a5d2c72c9ad5f274c7ac5e9
Jip <Jip@ip-145-93-148-169.workstations.fontys.nl>
2016-11-02 09:40:31 +0100

    Revert "Fixed FX:ID crap"
    This reverts commit 28ae72bd45e848fda9867d8a8b7e222a8eef6170.

--- src/main/java/edu/fontys/cims/AlertTab_Controller.java
+++ src/main/java/edu/fontys/cims/AlertTab_Controller.java
@@ -73,11 +73,15 @@ public class AlertTab_Controller implements Initializable {
 
     private final ObservableList<InitRequest.Alert> alerts = FXCollections.observableArrayList();
 
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        //mapView.addMapInializedListener(this);
+
         InitRequest.InitResponse resp = null;
         resp = Api.init();
         
@@ -114,6 +118,7 @@ public class AlertTab_Controller implements Initializable {
                 SceneFXMLController.map.addMarker(SceneFXMLController.marker);
                 SceneFXMLController.map.panTo(pos);
                 txtAlertUserDescription.setText(newValue.getUserDescription());
+
             }
         });
 
@@ -128,6 +133,7 @@ public class AlertTab_Controller implements Initializable {
 
             }
         });
+
     }
 
     @FXML
